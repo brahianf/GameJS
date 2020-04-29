9a9927e39ee8eb194775433088e5c35415bba147
brahianf <brahianf@unicauca.edu.co>
2020-04-28 21:32:52 -0500

    Obtener input del usuario evento click

--- js/simonJuego.js
+++ js/simonJuego.js
@@ -5,6 +5,7 @@ const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
 
 class Juego{
+
   constructor() {
     this.inicializar()
     this.generarSecuencia()
@@ -12,8 +13,9 @@ class Juego{
   }
 
   inicializar() {
+    this.elegirColor = this.elegirColor.bind(this)
     btnEmpezar.classList.add('hide')
-    this.nivel=7
+    this.nivel=1
     this.colores = {
       celeste,
       violeta,
@@ -28,6 +30,7 @@ class Juego{
 
   siguienteNivel(){
     this.iluminarSecuecia()
+    this.agregarEventosClick()
   }
 
   transformarNumeroAcolor(numero){
@@ -58,7 +61,33 @@ class Juego{
   apagarColor(color){
     this.colores[color].classList.remove('light')
   }
+
+  // JavaScript le agrega evento clic a  los colores
+  // le dice al navegador que funcion ejecutar asincronamente 
+  // cuando  se realize evento click 
+  // navegador le avisa a JS que ejecute la funcion 
+  // cuando Js termine todas sus tareas la  va a ajecutar
+
+  agregarEventosClick(){
+    // var self = this  
+    // para que javaScrip no pierda el contexto con el Listener, 
+    // en este caso el this, esta referenciado al evento click
+    // Luego del evento el this pasa a ser el elemento html <div>
+    // y no  Juego {nivel: 1, colores: {…}, secuencia: Array(10)}
+    // this.colores.celeste.addEventListener('click', this.elegirColor.bind(self))
+    // o se amarra con .bind al this del juego 
+
+    this.colores.celeste.addEventListener('click', this.elegirColor)
+    this.colores.verde.addEventListener('click', this.elegirColor)
+    this.colores.violeta.addEventListener('click', this.elegirColor)
+    this.colores.naranja.addEventListener('click', this.elegirColor)
+  }
+
+  elegirColor(ev){
+    console.log(this)
+  }
 }
+
 
 function empezarJuego() {
   window.juego = new Juego()
