3a49aed30c7ed51443d03b0ed25e2447e3f21df4
brahianf <brahianf@unicauca.edu.co>
2020-04-29 02:09:33 -0500

    Agregando verificacion del color elegido, estados finales y SweetAlerts

--- js/simonJuego.js
+++ js/simonJuego.js
@@ -3,18 +3,21 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
+const ULTIMO_NIVEL = 1
 
 class Juego{
 
   constructor() {
     this.inicializar()
     this.generarSecuencia()
-    this.siguienteNivel()
+    setTimeout(this.siguienteNivel(), 500)
+    
   }
 
   inicializar() {
+    // this.siguienteNivel = this.siguienteNivel.bind(this)
     this.elegirColor = this.elegirColor.bind(this)
-    btnEmpezar.classList.add('hide')
+    this.toggleBtnEmpezar()
     this.nivel=1
     this.colores = {
       celeste,
@@ -24,11 +27,20 @@ class Juego{
     }
   }
 
+  toggleBtnEmpezar(){
+    if (btnEmpezar.classList.contains('hide')) {
+      btnEmpezar.classList.remove('hide')
+    } else {
+      btnEmpezar.classList.add('hide')
+    }
+  }
+
   generarSecuencia(){
-    this.secuencia= new Array(10).fill(0).map(n => Math.floor(Math.random() *4))
+    this.secuencia= new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() *4))
   }
 
   siguienteNivel(){
+    this.subnivel=0
     this.iluminarSecuecia()
     this.agregarEventosClick()
   }
@@ -43,6 +55,19 @@ class Juego{
         return 'naranja'
       case 3:
         return 'verde'
+    }
+  }
+
+  transformarColoraNumero(color){
+    switch(color){
+      case 'celeste':
+        return 0
+      case 'violeta':
+        return 1
+      case 'naranja':
+        return 2
+      case 'verde':
+        return 3
     }
   }
 
@@ -83,8 +108,50 @@ class Juego{
     this.colores.naranja.addEventListener('click', this.elegirColor)
   }
 
+  eliminarEventosClick(){
+    this.colores.celeste.removeEventListener('click', this.elegirColor)
+    this.colores.verde.removeEventListener('click', this.elegirColor)
+    this.colores.violeta.removeEventListener('click', this.elegirColor)
+    this.colores.naranja.removeEventListener('click', this.elegirColor)
+  }
+
   elegirColor(ev){
-    console.log(this)
+    // En las etiquetas div de cada color se agrego el atributo data-color="celeste"
+    // el cual se incluye en el dataSet del target
+    const nombreColor= ev.target.dataset.color
+    const numeroColor =  this.transformarColoraNumero(nombreColor)
+    this.iluminarColor(nombreColor)
+    if (numeroColor === this.secuencia[this.subnivel]){
+      this.subnivel++
+      if (this.subnivel === this.nivel){
+        this.nivel++
+        this.eliminarEventosClick()
+        if (this.nivel === (ULTIMO_NIVEL + 1)){
+          // Win
+          this.ganoJuego()
+        } else {
+          // Js con setTimeout encarga al Navegador de llamar ejecutar funcion y ahora el this es window
+          // Se amarra el this del juego al contexto de nuevo
+          setTimeout(this.siguienteNivel.bind(this),1500)
+        }
+      }
+    } else {
+      // Losser
+      this.perdioJuego()
+    }
+  }
+
+  ganoJuego(){
+    swal('Simon Dice que', 'Ganaste el Juego', 'succes')
+    .then(this.inicializar.bind(this))
+  }
+
+  perdioJuego(){
+    swal('Simon Dice que', 'Losserrrr', 'error')
+    .then(() => {
+      this.eliminarEventosClick()
+      this.inicializar()
+    })
   }
 }
 
